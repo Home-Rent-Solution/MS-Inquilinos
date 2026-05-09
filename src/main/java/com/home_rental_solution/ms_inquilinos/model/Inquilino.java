05078d489c25bec2c2cde4283cae530a5ba9c45f
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-09 09:13:24 -0400

    Se implementa la clase modelo para inquilinos: - Añadida entidad Inquilino con atributos de perfil y contacto. - Implementada lista de historial de reservas para seguimiento del usuario. - Aplicadas validaciones de integridad de datos (@NotBlank, @Email, @NotNull).

--- src/main/java/com/home_rental_solution/ms_inquilinos/model/Inquilino.java
+++ src/main/java/com/home_rental_solution/ms_inquilinos/model/Inquilino.java
@@ -1,4 +1,29 @@
 package com.home_rental_solution.ms_inquilinos.model;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Inquilino {
+
+    @NotNull(message = "El ID del Inquilino no debe estar vacio")
+    private Integer idInquilino;
+
+    @NotBlank(message = "El nombre es obligatorio")
+    private String nombre;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ingresar un email valido")
+    private String email;
+
+    @NotNull(message = "El historial de reservas no puede ser nulo")
+    private List<String> historialReservas;
 }
