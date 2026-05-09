35ad96913a258d9ff38c3acb5443292833d3452e
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-09 08:56:39 -0400

    Creacion del package model

---
+++ src/main/java/com/home_rental_solution/ms_inquilinos/MsInquilinosApplication.java
@@ -0,0 +1,13 @@
+package com.home_rental_solution.ms_inquilinos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsInquilinosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsInquilinosApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/home_rental_solution/ms_inquilinos/model/Inquilino.java
@@ -0,0 +1,4 @@
+package com.home_rental_solution.ms_inquilinos.model;
+
+public class Inquilino {
+}
