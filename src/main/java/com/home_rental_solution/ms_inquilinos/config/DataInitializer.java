c8e61622e8cd5b6bde0fb0663b0344051d6a1476
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-10 20:55:25 -0400

    Se configura inicializacion de datos para inquilinos: - Añadida clase DataInitializer con soporte para CommandLineRunner. - Configurada inserción de inquilinos de prueba con sus respectivos historiales de reserva. - Implementada validación de base de datos vacía para evitar duplicidad. - Integrado logging con Slf4j para seguimiento del proceso de seeding.

--- src/main/java/com/home_rental_solution/ms_inquilinos/config/DataInitializer.java
+++ src/main/java/com/home_rental_solution/ms_inquilinos/config/DataInitializer.java
@@ -1,4 +1,56 @@
 package com.home_rental_solution.ms_inquilinos.config;
 
-public class DataInitializer {
+import com.home_rental_solution.ms_inquilinos.model.Inquilino;
+import com.home_rental_solution.ms_inquilinos.repository.InquilinoRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DataInitializer implements CommandLineRunner{
+
+    private final InquilinoRepository inquilinoRepository;
+
+    @Override
+    public void run(String...args){
+        if (inquilinoRepository.count() > 0){
+            log.info(">>> DataInitializer: la BD ya tiene datos, se omite la carga inicial");
+            return;
+        }
+        log.info(">>> Datainitializer: BD vacia detectado, insertando inquilinos de prueba");
+        inquilinoRepository.save(new Inquilino(
+                null,
+                "Pedro Gonzalex",
+                "pedro.gonzalez@email.com",
+                new ArrayList<>(Arrays.asList(
+                        "Reserva en Santiago Centro",
+                        "Reserva en Providencia"
+                )),
+                false
+        ));
+        inquilinoRepository.save(new Inquilino(
+                null,
+                "Camila Rojas",
+                "camila.rojas@email.com",
+                new ArrayList<>(Arrays.asList(
+                        "Reserva en Maipu"
+                )),
+                false
+        ));
+
+        inquilinoRepository.save(new Inquilino(
+                null,
+                "Matias Silva",
+                "matias.silva@email.com",
+                new ArrayList<>(),
+                true
+        ));
+        log.info("DataInitializer: {} inquilinos insertados correctamente", inquilinoRepository.count());
+    }
 }
