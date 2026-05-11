d1a65bc041511574932cc6d7f84b441bdecb7b36
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-10 20:07:54 -0400

    Se implementa persistencia JPA en repository: - Reemplazada gestión de lista en memoria por JpaRepository. - Implementado Query Method para búsqueda de email ignore case. - Añadida validación optimizada para comprobar si un inquilino no está bloqueado. - Eliminada toda la lógica manual de iteración y manejo de IDs.

--- src/main/java/com/home_rental_solution/ms_inquilinos/repository/InquilinoRepository.java
+++ src/main/java/com/home_rental_solution/ms_inquilinos/repository/InquilinoRepository.java
@@ -1,118 +1,17 @@
 package com.home_rental_solution.ms_inquilinos.repository;
 
 import com.home_rental_solution.ms_inquilinos.model.Inquilino;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.ArrayList;
-
 @Repository
-public class InquilinoRepository {
-
-    private List<Inquilino> listaInquilinos = new ArrayList<>();
-    private int contadorId = 4;
-
-    public InquilinoRepository(){
-        listaInquilinos.add(new Inquilino(
-                1,
-                "Pedro gonzalez",
-                "pedro.gonzalez@email.com",
-                new ArrayList<>(Arrays.asList(
-                        "Reserva en Santiago Centro",
-                        "Reserva en Providencia"
-                )),
-                false
-        ));listaInquilinos.add(new Inquilino(
-                2,
-                "Camila Rojas",
-                "camila.rojas@email.com",
-                new ArrayList<>(Arrays.asList(
-                        "Reserva en Maipu"
-                )),
-                false
-        ));listaInquilinos.add(new Inquilino(
-                3,
-                "Matias Silva",
-                "matias.silva@email.com",
-                new ArrayList<>(),
-                false
-                ));
-    }
-
-    //CRUD
-    //obtener todos los inquilinos
-    public List<Inquilino> obtenerInquilinos(){
-        return listaInquilinos;
-    }
-
-    //buscar por I
-    public Inquilino obtenerPorId(int idInquilino){
-        for (Inquilino inquilino : listaInquilinos){
-            if (inquilino.getIdInquilino() == idInquilino){
-                return inquilino;
-            }
-        }
-        return null;
-    }
-
-    //guardar nuevo inquilino
-    public Inquilino guardar(Inquilino inquilino){
-        inquilino.setIdInquilino(contadorId++);
-        listaInquilinos.add(inquilino);
-        return inquilino;
-    }
-
-    //actualizar inquilino
-    public Inquilino actualizar(Inquilino inquilino){
-        for (int i = 0; i < listaInquilinos.size(); i++){
-            if (listaInquilinos.get(i).getIdInquilino().equals(inquilino.getIdInquilino())){
-                listaInquilinos.set(i, inquilino);
-                return inquilino;
-            }
-        }
-        return null;
-    }
-
-    //eliminar por ID
-    public void eliminar(int idInquilino){
-        Inquilino inquilino = obtenerPorId(idInquilino);
-        if (inquilino != null){
-            listaInquilinos.remove(inquilino);
-        }
-    }
+public interface InquilinoRepository extends JpaRepository<Inquilino, Integer> {
 
     //***EXTRAS***
     //buscar por email
-    public Inquilino obtenerPorEmail(String email){
-        for (Inquilino inquilino : listaInquilinos){
-            if (inquilino.getEmail().equalsIgnoreCase(email)){
-                return inquilino;
-            }
-        }
-        return null;
-    }
+    Inquilino findByEmailIgnoreCase(String email);
 
-    // obtener historial
-    public List<String> obtenerHistorial(int idInquilino){
-        Inquilino inquilino = obtenerPorId(idInquilino);
-        if (inquilino != null){
-            return inquilino.getHistorialReservas();
-        }
-        return null;
-    }
+    //validar si esta puede arrendar
+    boolean existsByIdInquilinoAndBloqueadoFalse(Integer idInquilino);
 
-    //validar si esta registrado
-    public boolean estaRegistrado(int idInquilino){
-        return obtenerPorId(idInquilino) != null;
-    }
-
-    //bloquear a un inquilino
-    public Inquilino bloquear(int idInquilino){
-        Inquilino inquilino = obtenerPorId(idInquilino);
-        if (inquilino != null){
-            inquilino.setBloqueado(true);
-        }
-        return inquilino;
-    }
 }
