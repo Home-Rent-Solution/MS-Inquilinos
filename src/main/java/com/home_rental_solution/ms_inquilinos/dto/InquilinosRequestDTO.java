d63789d41fc272d550776664d946922a7d68a3cd
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-12 15:29:29 -0400

    Se crea DTO de entrada para Inquilinos
    - Movidas las validaciones de Jakarta al RequestDTO.
    - Excluidos campos sensibles y automáticos (id, bloqueado, historial).

---
+++ src/main/java/com/home_rental_solution/ms_inquilinos/dto/InquilinosRequestDTO.java
@@ -0,0 +1,29 @@
+package com.home_rental_solution.ms_inquilinos.dto;
+
+import jakarta.persistence.Column;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InquilinosRequestDTO {
+
+    //idInquilino no se incluye porque MySQL lo genera
+    //bloqueado no se incluye porque se maneja con put
+    //historialReserva no se incluye porque el cliente no lo maneja
+
+    @NotBlank(message = "El nombre es obligatorio")
+    @Size(max = 120, message = "El nombre no puede superar los 120 caracteres")
+    private String nombre;
+
+    @NotBlank(message = "El email es obligatorio")
+    @Email(message = "Debe ingresar un email valido")
+    @Column(nullable = false, unique = true, length = 150)
+    private String email;
+
+}
---
+++ src/main/java/com/home_rental_solution/ms_inquilinos/dto/InquilinosResponseDTO.java
@@ -0,0 +1,13 @@
+package com.home_rental_solution.ms_inquilinos.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class InquilinosResponseDTO {
+
+    //idInquilino no se incluye porque MySQL lo genera
+}
