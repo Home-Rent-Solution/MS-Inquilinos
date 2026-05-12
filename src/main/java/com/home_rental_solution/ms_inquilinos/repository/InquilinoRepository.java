d2b99320705da9d8d4f2da800adb920513831e12
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-12 19:34:00 -0400

    Se estandariza capa de persistencia en ms-inquilinos mediante el uso de @Query

--- src/main/java/com/home_rental_solution/ms_inquilinos/repository/InquilinoRepository.java
+++ src/main/java/com/home_rental_solution/ms_inquilinos/repository/InquilinoRepository.java
@@ -2,6 +2,8 @@ package com.home_rental_solution.ms_inquilinos.repository;
 
 import com.home_rental_solution.ms_inquilinos.model.Inquilino;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -12,7 +14,8 @@ public interface InquilinoRepository extends JpaRepository<Inquilino, Integer> {
 
     //***EXTRAS***
     //buscar por email
-    Optional<Inquilino> findByEmailIgnoreCase(String email);
+    @Query("SELECT i FROM Inquilino i WHERE LOWER(i.email) = LOWER(:email)")
+    Optional<Inquilino> findByEmailIgnoreCase(@Param("email") String email);
 
     //verificar si el email ya existe
     boolean existsByEmailIgnoreCase(String email);
@@ -21,7 +24,9 @@ public interface InquilinoRepository extends JpaRepository<Inquilino, Integer> {
     boolean existsByIdInquilinoAndBloqueadoFalse(Integer idInquilino);
 
     //Listar por estado de bloqueo
+    @Query("SELECT i FROM Inquilino i WHERE i.bloqueado = false ORDER BY i.nombre")
     List<Inquilino> findByBloqueadoFalse();
-    List<Inquilino> findByBloqueadoTrue();
 
+    @Query("SELECT i FROM Inquilino i WHERE i.bloqueado = true ORDER BY i.nombre")
+    List<Inquilino> findByBloqueadoTrue();
 }
