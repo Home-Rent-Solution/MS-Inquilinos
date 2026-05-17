117406ba6abf16108f633964e3e58479076d4d8a
victorurravalenzuela <vi.urrav@duocuc.cl>
2026-05-16 21:08:40 -0400

    Modificaciones finales

--- src/main/java/com/home_rental_solution/ms_inquilinos/client/PropiedadClient.java
+++ src/main/java/com/home_rental_solution/ms_inquilinos/client/PropiedadClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "ms-propiedades", url = "${ms.propiedades.url")
+@FeignClient(name = "ms-propiedades", url = "${ms.propiedades.url}")
 public interface PropiedadClient {
 
     //validar si la propiedad existe y esta disponible
